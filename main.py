97261640fd545b247244903b0bef6e3c9dcd7d19
Abm32 <110992125+Abm32@users.noreply.github.com>
2023-01-08 00:07:50 +0530

    updated

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import csv
-import pandas
- 
-# reading the CSV file
-csvFile = pandas.read_csv('automobile.csv')
- 
-# displaying the contents of the CSV file
-print(csvFile)
+# Load the Pandas libraries with alias 'pd' 
+import pandas as pd 
+# Read data from file 'filename.csv' 
+# (in the same directory that your python process is based)
+# Control delimiters, rows, column names with read_csv (see later) 
+data = pd.read_csv("automobile.csv") 
+# Preview the first 5 lines of the loaded data 
+name = data['NAME'].tolist()
+phone = data['Phone'].tolist()
+dist = data['distance'].tolist()
+
+user = input('Query: ')
+
+help1 = ["help","Help","HELP"]
+
+for ele in help1:
+    if user == ele:
+        user2 = input('Emergency Servic: ')
